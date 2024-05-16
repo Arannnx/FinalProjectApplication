a7bd1aea809de0613379988805cfbab67bac30cd
Arannnx <gitallancorpuz@gmail.com>
2024-05-16 21:40:12 +0800

    halfway done

--- app/src/main/java/com/example/finalprojectapplication/OngoingEvents.java
+++ app/src/main/java/com/example/finalprojectapplication/OngoingEvents.java
@@ -1,24 +1,35 @@
 package com.example.finalprojectapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.cardview.widget.CardView;
 
 public class OngoingEvents extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ongoing_events);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        CardView ongoingCard = findViewById(R.id.ongoings); // Assuming the ID is 'ongoings' from your XML
+
+        ongoingCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)  {
+                openongoingCard();
+            }
         });
+
     }
+
+    private void openongoingCard() {
+        Intent intent = new Intent(this, OngoingEvents.class); // Ensure Ongoing is a valid Activity class
+        startActivity(intent);
+    }
+
+
 }
